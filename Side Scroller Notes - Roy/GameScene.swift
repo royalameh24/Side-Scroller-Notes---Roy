40c0d39aa3e4c7fc8400b1aa0a0828c230cae62a
Roy Alameh <roy.alameh24@d155.org>
2023-01-30 09:35:22 -0600

    First significant commit

--- Side Scroller Notes - Roy/GameScene.swift
+++ Side Scroller Notes - Roy/GameScene.swift
@@ -9,80 +9,33 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var character : SKSpriteNode!
+    private var ground : SKSpriteNode!
+    let cam = SKCameraNode()
+    let speedFactor = 1
     
     override func didMove(to view: SKView) {
+        //sets orientation of info-p list
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        character = self.childNode(withName: "guy") as? SKSpriteNode
+        character.physicsBody?.velocity = CGVector(dx: 0, dy: 0.0)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        ground = self.childNode(withName: "ground") as? SKSpriteNode
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        self.camera = cam
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    override func update(_ currentTime: TimeInterval) {
+        cam.position = character.position
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        var speed = 0
+        for touch in touches {
+            speed = CGFloat(touch.position.x as! Int) - self.childNode(withName: "guy")?.position.x as! Int
+        }
+        self.childNode(withName: "guy")?.physicsBody?.velocity = CGVector(dx: speed, dy: self.childNode(withName: "guy")?.physicsBody?.velocity.dy)
     }
 }
--- Side Scroller Notes - Roy/GameViewController.swift
+++ Side Scroller Notes - Roy/GameViewController.swift
@@ -10,7 +10,9 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
+    var play : GameScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,7 +21,7 @@ class GameViewController: UIViewController {
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
-                
+                play = scene as? GameScene
                 // Present the scene
                 view.presentScene(scene)
             }
@@ -32,13 +34,13 @@ class GameViewController: UIViewController {
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        .landscapeLeft
     }
 
+    @IBAction func jumpAction(_ sender: UIButton) {
+        play.childNode(withName: "guy")?.physicsBody?.velocity = CGVector(dx: play.childNode(withName: "guy")?.physicsBody?.velocity.dx ?? 30, dy: 500)
+    }
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
